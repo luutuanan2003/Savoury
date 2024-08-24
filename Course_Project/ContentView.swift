546bc89514461e569f3ca0488a8fff44c14de6cf
Kien Le <s3878260@rmit.edu.vn>
2024-08-24 18:29:11 +1000

    Initial Commit

---
+++ Course_Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Course_Project
+//
+//  Created by Elwiz Scott on 24/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Course_Project/Course_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Course_ProjectApp.swift
+//  Course_Project
+//
+//  Created by Elwiz Scott on 24/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Course_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
